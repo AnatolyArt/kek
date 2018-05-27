f6ab3cf25ade1d042966ac31249a19e7ac4eec38
AnatolyArt <artemenko.ay@yandex.ru>
2018-05-27 23:35:44 +0300

    clear interests page

--- src/views/dictionaries/Lifestyles.vue
+++ src/views/dictionaries/Lifestyles.vue
@@ -1,8 +1,7 @@
 <template>
     <div>
         <b-card :header="caption">
-            <h4 slot="header"
-                class="mb-0">{{caption}}<a type="button" href="#/dictionaries/categories/add" class="btn btn-sm btn-success pull-right m-0">Add new</a></h4>
+            <h4 slot="header" class="mb-0">{{caption}}</h4>
             <b-table responsive="sm" :items="items" :fields="fields" ref="table">
                 <template slot="name" slot-scope="data">
                     {{ data.item.name }}
@@ -12,7 +11,7 @@
                     <i class="fa fa-arrow-up fa-lg" v-on:click="replaceRow(row.index, 1)"></i>
                 </template>
                 <template slot="edit" slot-scope="data">
-                    <a :href="`#/dictionaries/categories/edit/${data.item.id}`" ><i class="fa fa-pencil fa-lg"></i></a>
+                    <a :href="`#/dictionaries/interests/edit/${data.item.id}`" ><i class="fa fa-pencil fa-lg"></i></a>
                 </template>
             </b-table>
         </b-card>
@@ -62,57 +61,7 @@
       }
     },
     methods: {
-      getItems(){
-        return this.items;
-      },
-      getRowCount (items) {
-        return items.length
-      },
-      deleteCategoryPopup(catId){
-        let category = this.items.find(x => parseInt(x.id) === parseInt(catId));
-        this.delete = catId;
-        if(category.interests === 0){
-          this.canDelete = true;
-        }else{
-          this.errorDelete = true;
-        }
-      },
-      deleteCategory(){
-        if(this.delete !== 0){
-          this.$root.ajax.delete('interests/categories/' + this.delete)
-            .then((response) => {
-              let index = this.items.findIndex(x => parseInt(x.id) === parseInt(this.delete));
-              this.$delete(this.items, index);
-              this.delete = 0;
-            }).catch(function (error) {
-            alert(error);
-          });
-        }
-      },
-      replaceRow(row, move){
-        var t = this.items[row];
-        // move up
-        if(move === 1){
-          if(row !== 0){
-            this.$set(this.items, row, this.items[row - 1]);
-            this.$set(this.items, row - 1, t);
-          }
-        }else{
-          if(row !== this.items.length - 1){
-            this.$set(this.items, row, this.items[row + 1]);
-            this.$set(this.items, row + 1, t);
-          }
-        }
 
-        var data = {
-          'ids': this.items.map(item => item.id)
-        }
-        this.$root.ajax.post('interests/categories/reoder', data)
-          .then((response) => {
-          }).catch(function (error) {
-          alert(error);
-        });
-      }
     }
   }
 </script>
